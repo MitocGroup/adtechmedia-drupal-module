8f13350689bc46dd66ff95ece0f1fdaa2eaf7832
vcernomschi <vcernomschi@mitocgroup.com>
2016-09-20 13:45:33 +0300

    Update scripts

--- bin/test/install.sh
+++ bin/test/install.sh
@@ -8,7 +8,7 @@ source $(dirname $0)/_head.sh
 ########################
 ### Install NPM deps ###
 ########################
-__CMD="npm install"
+__CMD="sudo npm install"
 
 subpath_run_cmd "${__SRC_PATH}" "$__CMD" "$__CMD" "${1}"
 
