2fc43a3bd46c4c0254b5aed73ea1f7e7e3b9e69e
Sorin Dediu <sorindediu21@gmail.com>
2016-10-02 22:32:58 +0300

    Override ATM config service form markup to style it

--- src/Form/AdTechMediaConfigForm.php
+++ src/Form/AdTechMediaConfigForm.php
@@ -72,17 +72,33 @@ class AdTechMediaConfigForm extends ConfigFormBase {
       '#title' => $this->t('API Key'),
       '#default_value' => $config->get('api_key'),
       '#required' => TRUE,
+      '#title_display' => 'after',
+      '#suffix' => '<span class="bar"></span>',
     );
 
-    $form['general']['generate'] = array(
+    $form['general']['regenerate'] = array(
       '#type' => 'button',
       '#value' => $this->t('Regenerate'),
+      '#attributes' => array(
+        'class' => array('btn', 'regenerate'),
+      ),
       '#ajax' => array(
         'callback' => array($this, 'regenerateApiKeyCallback'),
         'event' => 'click',
         'wrapper' => 'edit-api-key',
         'method' => 'replaceWith',
       ),
+      //'#prefix' => '<i class="mdi mdi-autorenew"></i>',
+    );
+
+    $form['general']['country'] = array(
+      '#type' => 'select',
+      '#title' => $this->t('Country'),
+      '#options' => array(
+        'usa' => $this->t('USA'),
+        'md' => $this->t('Moldova')
+      ),
+      '#default_value' => $config->get('country'),
     );
 
     $form['general']['revenue_model'] = array(
@@ -97,20 +113,25 @@ class AdTechMediaConfigForm extends ConfigFormBase {
       '#description' => $this->t('Select revenue model'),
     );
 
-    $form['general']['country'] = array(
-      '#type' => 'select',
-      '#title' => $this->t('Country'),
-      '#options' => array(
-        'usa' => $this->t('USA'),
-        'md' => $this->t('Moldova')
-      ), //@todo from api.
-      '#default_value' => $config->get('country'),
-    );
-
     $form['general']['email'] = array(
       '#type' => 'email',
-      '#title' => $this->t('Email'),
+      '#title' => $this->t('Email Address'),
       '#default_value' => $config->get('email'),
+      '#title_display' => 'after',
+    );
+
+    $form['general']['activate'] = array(
+      '#type' => 'button',
+      '#value' => $this->t('Activate'),
+      '#attributes' => array(
+        'class' => array('btn', 'activate'),
+      ),
+//      '#ajax' => array(
+//        'callback' => array($this, 'regenerateApiKeyCallback'),
+//        'event' => 'click',
+//        'wrapper' => 'edit-api-key',
+//        'method' => 'replaceWith',
+//      ),
     );
 
     $form['content'] = array(
@@ -119,27 +140,13 @@ class AdTechMediaConfigForm extends ConfigFormBase {
       '#open' => TRUE,
     );
 
-    $form['content']['content_paywall'] = array(
-      '#type' => 'select',
-      '#title' => $this->t('Content Paywall'),
-      '#options' => array(
-        '3_transactions' => $this->t('3 transactions'),
-        '100_cents' => $this->t('100 cents'),
-      ),
-      '#default_value' => $config->get('content_paywall'),
-      '#description' => $this->t('Remove paywall after.'),
-    );
-
     $form['content']['content_pricing'] = array(
-      '#type' => 'select',
-      '#title' => $this->t('Content Pricing'),
-      '#options' => array(
-        'autopricing' => $this->t('Autopricing'),
-        29 => $this->t('29 cents'),
-        49 => $this->t('49 cents'),
-      ),
+      '#type' => 'textfield',
+      '#title' => $this->t('Content pricing'),
       '#default_value' => $config->get('content_pricing'),
-      '#description' => $this->t('Select content price.'),
+      '#description' => $this->t('It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters'),
+      '#placeholder' => $this->t('number'),
+      '#title_display' => 'after',
     );
 
     $form['content']['content_currency'] = array(
@@ -153,20 +160,47 @@ class AdTechMediaConfigForm extends ConfigFormBase {
       '#description' => $this->t('Select content currency.'),
     );
 
-    $form['content']['content_preview'] = array(
+    $form['content']['content_paywall'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Content paywall'),
+      '#default_value' => $config->get('content_paywall'),
+      '#description' => $this->t('Remove paywall after.'),
+      '#placeholder' => $this->t('number'),
+      '#title_display' => 'after',
+    );
+
+    $form['content']['content_paywall_type'] = array(
       '#type' => 'select',
-      '#title' => $this->t('Content Preview'),
+      '#title' => $this->t('Content paywall type'),
       '#options' => array(
-        '100_words' => $this->t('100 words'),
-        '3_paragraphs' => $this->t('3 paragraphs'),
+        'transactions' => $this->t('Transactions'),
       ),
+      '#default_value' => $config->get('content_paywall_type'),
+      '#description' => $this->t('Select preview type.'),
+    );
+
+    $form['content']['content_preview'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Content preview'),
       '#default_value' => $config->get('content_preview'),
       '#description' => $this->t('Select how content should be displayed.'),
+      '#placeholder' => $this->t('number'),
+      '#title_display' => 'after',
+    );
+
+    $form['content']['content_preview_type'] = array(
+      '#type' => 'select',
+      '#title' => $this->t('Content preview type'),
+      '#options' => array(
+        'paragraph' => $this->t('Paragraph'),
+      ),
+      '#default_value' => $config->get('content_preview_type'),
+      '#description' => $this->t('Select preview type.'),
     );
 
     $form['content']['locking_algorithm'] = array(
       '#type' => 'select',
-      '#title' => $this->t('Content Locking Algorithm'),
+      '#title' => $this->t('Content unlocking algorithm'),
       '#options' => array(
         'blur' => $this->t('Blur'),
         'scramble' => $this->t('Scramble'),
@@ -182,17 +216,15 @@ class AdTechMediaConfigForm extends ConfigFormBase {
       '#title' => $this->t('DNS access'),
       '#default_value' => $config->get('dns_access'),
       '#description' => $this->t('Route 53 AWS key.'),
+      '#title_display' => 'after',
     );
 
     $form['content']['social_media'] = array(
-      '#type' => 'select',
-      '#title' => $this->t('Social Media Access'),
-      '#options' => array(
-        'facebook' => $this->t('Facebook'),
-        'twitter' => $this->t('Twitter'),
-      ),
+      '#type' => 'textfield',
+      '#title' => $this->t('Social media credentials'),
       '#default_value' => $config->get('social_media'),
       '#description' => $this->t('Select social media.'),
+      '#title_display' => 'after',
     );
 
     $form['template'] = array(
@@ -238,6 +270,8 @@ class AdTechMediaConfigForm extends ConfigFormBase {
         '#rows' => 14,
       );
     }
+
+    $form['#attached']['library'][] = 'adtechmedia/adtechmedia.api';
 
     return parent::buildForm($form, $form_state);
   }
