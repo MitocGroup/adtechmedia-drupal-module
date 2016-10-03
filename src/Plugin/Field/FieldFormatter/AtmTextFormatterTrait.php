024280e324d5d3539ed4f46b4fd8cf9d1ddf51e9
Sorin Dediu <sorindediu21@gmail.com>
2016-10-04 01:46:29 +0300

    Process text with ATM service and return locked content.

--- src/Plugin/Field/FieldFormatter/AtmTextFormatterTrait.php
+++ src/Plugin/Field/FieldFormatter/AtmTextFormatterTrait.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace Drupal\adtechmedia\Plugin\Field\FieldFormatter;
+use Drupal\adtechmedia\AtmClient;
+use Drupal\Core\Entity\EntityInterface;
 
 /**
  * Provides helper methods to unlock content via ATM Service.
@@ -8,17 +10,40 @@ namespace Drupal\adtechmedia\Plugin\Field\FieldFormatter;
 trait AtmTextFormatterTrait {
 
   /**
-   * @todo lock the content and apply unlocking specific algorithm.
-   */
-  public static function atmContentLock($text) {
-    $text = 'This content is locked. </br>' . $text;
-    return $text;
-  }
-
-  /**
+   * Process text with ATM Service and apply locking algorithm.
    *
+   * @param $text
+   *   The text to be processed.
+   * @param \Drupal\Core\Entity\EntityInterface $entity
+   *   The entity parent.
+   *
+   * @return string
+   *   Processed text by scrambling or blurring it.
    */
-  public static function atmContentUnlock($text) {
+  public static function atmContentProcess($text, EntityInterface $entity) {
+    // Create a content identifier.
+    $content_id = implode('.', [
+      $entity->getEntityTypeId(),
+      $entity->bundle(),
+      $entity->id(),
+    ]);
+
+    // Get property from config.
+    $atm_settings = \Drupal::configFactory()->get('adtechmedia.settings');
+    $property_id = $atm_settings->get('locking_algorithm');
+
+    $client = new AtmClient();
+    $locked_text = $client->retrieveLockedContent($content_id, $property_id);
+
+    if (empty($locked_text)) {
+      if ($client->createLockedContent($content_id, $property_id, $text)) {
+        $locked_text = $client->retrieveLockedContent($content_id, $property_id);
+      }
+    }
+
+    if (!empty($locked_text['Content'])) {
+      $text = $locked_text['Content'];
+    }
 
     return $text;
   }
