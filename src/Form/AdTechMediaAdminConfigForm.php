897ee52e5d7d053cb611c622691e6be3cf914b00
Sorin Dediu <sorindediu21@gmail.com>
2016-10-05 23:32:30 +0300

    Add configurable host and base url in config form

--- src/Form/AdTechMediaAdminConfigForm.php
+++ src/Form/AdTechMediaAdminConfigForm.php
@@ -21,40 +21,82 @@ class AdTechMediaAdminConfigForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function getEditableConfigNames() {
-    return ['adtechmedia.admin_settings'];
+    return ['adtechmedia.settings'];
   }
 
   /**
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $config = $this->config('adtechmedia.admin_settings');
+    $config = $this->config('adtechmedia.settings');
 
-    $form['main'] = array(
+    $form['main'] = [
       '#type' => 'details',
       '#title' => $this->t('Main configuration'),
       '#open' => TRUE,
-    );
+    ];
 
-    $form['main']['use_atm'] = array(
+    $form['main']['use_atm'] = [
       '#type' => 'checkbox',
       '#title' => $this->t('Enable AdTechMedia'),
       '#default_value' => $config->get('use_atm'),
       '#description' => $this->t('Activate/Deactivate AdTechMedia service.'),
-    );
+    ];
 
-    $form['debug'] = array(
+    $form['main']['atm_host'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('ATM Host'),
+      '#default_value' => $config->get('atm_host'),
+      '#description' => $this->t('Configure AdTechMedia service host.'),
+      '#required' => TRUE,
+    ];
+
+    $form['main']['atm_base_path'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('ATM Base Path'),
+      '#default_value' => $config->get('atm_base_path'),
+      '#required' => TRUE,
+    ];
+
+    $form['debug'] = [
       '#type' => 'details',
       '#title' => $this->t('Development'),
-    );
+    ];
 
-    $form['debug']['development'] = array(
+    $form['debug']['development'] = [
       '#type' => 'checkbox',
       '#title' => $this->t('Enable development mode'),
       '#default_value' => $config->get('development'),
-    );
+    ];
+
+    $form['debug']['atm_dev_host'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('ATM Dev Host'),
+      '#default_value' => $config->get('atm_dev_host'),
+      '#description' => $this->t('Configure AdTechMedia development service host.'),
+    ];
+
+    $form['debug']['atm_dev_base_path'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('ATM Dev Base Path'),
+      '#default_value' => $config->get('atm_dev_base_path'),
+    ];
 
     return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $config = $this->config('adtechmedia.settings');
+
+    foreach ($form_state->getValues() as $key => $value) {
+      $config->set($key, $value);
+    }
+    $config->save();
+
+    parent::submitForm($form, $form_state);
   }
 
 }
--- src/Form/AdTechMediaConfigForm.php
+++ src/Form/AdTechMediaConfigForm.php
@@ -3,13 +3,11 @@
 namespace Drupal\adtechmedia\Form;
 
 use Drupal\adtechmedia\AtmClient;
-use Drupal\Component\Serialization\Json;
 use Drupal\Core\Ajax\AjaxResponse;
 use Drupal\Core\Ajax\InvokeCommand;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
-use GuzzleHttp\Client;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -63,222 +61,224 @@ class AdTechMediaConfigForm extends ConfigFormBase {
   public function buildForm(array $form, FormStateInterface $form_state) {
     $config = $this->config('adtechmedia.settings');
 
-    $form['general'] = array(
+    $form['general'] = [
       '#type' => 'details',
       '#title' => $this->t('General configuration'),
       '#open' => TRUE,
-    );
+    ];
 
-    $form['general']['api_key'] = array(
+    $form['general']['api_key'] = [
       '#type' => 'textfield',
       '#title' => $this->t('API Key'),
       '#default_value' => $config->get('api_key'),
       '#required' => TRUE,
       '#title_display' => 'after',
       '#suffix' => '<span class="bar"></span>',
-    );
+    ];
 
-    $form['general']['regenerate'] = array(
+    $form['general']['regenerate'] = [
       '#type' => 'button',
       '#value' => $this->t('Regenerate'),
-      '#attributes' => array(
-        'class' => array('btn', 'regenerate'),
-      ),
-      '#ajax' => array(
-        'callback' => array($this, 'regenerateApiKeyCallback'),
+      '#attributes' => [
+        'class' => ['btn', 'regenerate'],
+      ],
+      '#ajax' => [
+        'callback' => [$this, 'regenerateApiKeyCallback'],
         'event' => 'click',
         'wrapper' => 'edit-api-key',
         'method' => 'replaceWith',
-      ),
-      // '#prefix' => '<i class="mdi mdi-autorenew"></i>',.
-    );
+      ],
+      '#limit_validation_errors' => [],
+      '#submit' => [],
+    ];
 
-    $form['general']['country'] = array(
+    $form['general']['country'] = [
       '#type' => 'select',
       '#title' => $this->t('Country'),
-      '#options' => array(
+      '#options' => [
         'usa' => $this->t('USA'),
         'md' => $this->t('Moldova'),
-      ),
+      ],
       '#default_value' => $config->get('country'),
-    );
+    ];
 
-    $form['general']['revenue_model'] = array(
+    $form['general']['revenue_model'] = [
       '#type' => 'select',
       '#title' => $this->t('Revenue Model'),
-      '#options' => array(
+      '#options' => [
         'advertising' => $this->t('Advertising'),
         'micropayments' => $this->t('Micropayments'),
         'advertising_micropayments' => $this->t('Advertising & Micropayments'),
-      ),
+      ],
       '#default_value' => $config->get('revenue_model'),
       '#description' => $this->t('Select revenue model'),
-    );
+    ];
 
-    $form['general']['email'] = array(
+    $form['general']['email'] = [
       '#type' => 'email',
       '#title' => $this->t('Email Address'),
       '#default_value' => $config->get('email'),
       '#title_display' => 'after',
-    );
+    ];
 
-    $form['general']['activate'] = array(
+    $form['general']['activate'] = [
       '#type' => 'button',
       '#value' => $this->t('Activate'),
-      '#attributes' => array(
-        'class' => array('btn', 'activate'),
-      ),
-    );
+      '#attributes' => [
+        'class' => ['btn', 'activate'],
+      ],
+    ];
 
-    $form['content'] = array(
+    $form['content'] = [
       '#type' => 'details',
       '#title' => $this->t('Content configuration'),
       '#open' => TRUE,
-    );
+    ];
 
-    $form['content']['content_pricing'] = array(
+    $form['content']['content_pricing'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Content pricing'),
       '#default_value' => $config->get('content_pricing'),
       '#description' => $this->t('It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters'),
       '#placeholder' => $this->t('number'),
       '#title_display' => 'after',
-    );
+    ];
 
-    $form['content']['content_currency'] = array(
+    $form['content']['content_currency'] = [
       '#type' => 'select',
       '#title' => $this->t('Content currency'),
-      '#options' => array(
+      '#options' => [
         'usd' => $this->t('USD'),
         'mdl' => $this->t('MDL'),
-      ),
+      ],
       '#default_value' => $config->get('content_currency'),
       '#description' => $this->t('Select content currency.'),
-    );
+    ];
 
-    $form['content']['content_paywall'] = array(
+    $form['content']['content_paywall'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Content paywall'),
       '#default_value' => $config->get('content_paywall'),
       '#description' => $this->t('Remove paywall after.'),
       '#placeholder' => $this->t('number'),
       '#title_display' => 'after',
-    );
+    ];
 
-    $form['content']['content_paywall_type'] = array(
+    $form['content']['content_paywall_type'] = [
       '#type' => 'select',
       '#title' => $this->t('Content paywall type'),
-      '#options' => array(
+      '#options' => [
         'transactions' => $this->t('Transactions'),
-      ),
+      ],
       '#default_value' => $config->get('content_paywall_type'),
       '#description' => $this->t('Select preview type.'),
-    );
+    ];
 
-    $form['content']['content_preview'] = array(
+    $form['content']['content_preview'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Content preview'),
       '#default_value' => $config->get('content_preview'),
       '#description' => $this->t('Select how content should be displayed.'),
       '#placeholder' => $this->t('number'),
       '#title_display' => 'after',
-    );
+    ];
 
-    $form['content']['content_preview_type'] = array(
+    $form['content']['content_preview_type'] = [
       '#type' => 'select',
       '#title' => $this->t('Content preview type'),
-      '#options' => array(
-        'paragraph' => $this->t('Paragraph'),
-      ),
+      '#options' => [
+        'elements' => $this->t('Elements'),
+        'words' => $this->t('Words'),
+      ],
       '#default_value' => $config->get('content_preview_type'),
       '#description' => $this->t('Select preview type.'),
-    );
+    ];
 
-    $form['content']['locking_algorithm'] = array(
+    $form['content']['locking_algorithm'] = [
       '#type' => 'select',
       '#title' => $this->t('Content unlocking algorithm'),
-      '#options' => array(
+      '#options' => [
         'blur' => $this->t('Blur'),
         'scramble' => $this->t('Scramble'),
         'keywords' => $this->t('Keywords'),
         'blur_scramble' => $this->t('Blur & Scramble'),
-      ),
+      ],
       '#default_value' => $config->get('locking_algorithm'),
       '#description' => $this->t('How locked content should look.'),
-    );
+    ];
 
-    $form['content']['dns_access'] = array(
+    $form['content']['dns_access'] = [
       '#type' => 'textfield',
       '#title' => $this->t('DNS access'),
       '#default_value' => $config->get('dns_access'),
       '#description' => $this->t('Route 53 AWS key.'),
       '#title_display' => 'after',
-    );
+    ];
 
-    $form['content']['social_media'] = array(
+    $form['content']['social_media'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Social media credentials'),
       '#default_value' => $config->get('social_media'),
       '#description' => $this->t('Select social media.'),
       '#title_display' => 'after',
-    );
+    ];
 
-    $form['template'] = array(
+    $form['template'] = [
       '#type' => 'details',
       '#title' => $this->t('Templates management'),
       '#open' => TRUE,
-    );
+    ];
 
-    $form['template']['vertical_tabs'] = array(
+    $form['template']['vertical_tabs'] = [
       '#type' => 'vertical_tabs',
       '#default_tab' => 'edit-tab',
-    );
+    ];
 
-    $templates = array(
-      'pledge_view' => array(
+    $templates = [
+      'pledge_view' => [
         'title' => $this->t('Pledge View'),
         'name' => 'pledgeComponent',
-      ),
-      'ad_view' => array(
+      ],
+      'ad_view' => [
         'title' => $this->t('Advertising View'),
         'name' => 'adComponent',
-      ),
-      'pay_view' => array(
+      ],
+      'pay_view' => [
         'title' => $this->t('Pay View'),
         'name' => 'payComponent',
-      ),
-      'refund_view' => array(
+      ],
+      'refund_view' => [
         'title' => $this->t('Refund View'),
         'name' => 'refundComponent',
-      ),
-      'unlock_view' => array(
+      ],
+      'unlock_view' => [
         'title' => $this->t('Unlock View'),
         'name' => 'pledgeComponent',
-      ),
-      'price_view' => array(
+      ],
+      'price_view' => [
         'title' => $this->t('Price View'),
         'name' => 'payComponent',
-      ),
-    );
+      ],
+    ];
 
     foreach ($templates as $name => $template) {
-      $form['template']['tab_' . $name] = array(
+      $form['template']['tab_' . $name] = [
         '#type' => 'details',
         '#title' => $template['title'],
         '#group' => 'vertical_tabs',
-      );
+      ];
 
       $default_template = $config->get($name)['value'];
       if (empty($config->get($name)['value'])) {
         $default_template = self::getTemplate($template['name']);
       }
 
-      $form['template']['tab_' . $name][$name] = array(
+      $form['template']['tab_' . $name][$name] = [
         '#type' => 'text_format',
         '#format' => $config->get($name)['format'],
         '#default_value' => $default_template,
         '#rows' => 14,
-      );
+      ];
     }
 
     $form['#attached']['library'][] = 'adtechmedia/adtechmedia.api';
@@ -307,7 +307,8 @@ class AdTechMediaConfigForm extends ConfigFormBase {
    * Ajax callback to regenerate new api key.
    */
   public function regenerateApiKeyCallback($form, $form_state) {
-    $request = new AtmClient();
+    //$request = new AtmClient();
+    $request = $this->atmClient;
     $atm_response = $request->regenerateApiKey();
 
     $response = new AjaxResponse();
