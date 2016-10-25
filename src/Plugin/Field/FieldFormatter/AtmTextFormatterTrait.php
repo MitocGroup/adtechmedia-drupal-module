9a667809551bf46531fdc8bffa402f29c02d02e7
Sorin Dediu <sorindediu21@gmail.com>
2016-10-25 16:14:47 +0300

    Remove php content locking fallback

--- src/Plugin/Field/FieldFormatter/AtmTextFormatterTrait.php
+++ src/Plugin/Field/FieldFormatter/AtmTextFormatterTrait.php
@@ -22,26 +22,6 @@ trait AtmTextFormatterTrait {
    *   Processed text by scrambling or blurring it.
    */
   public static function atmContentProcess($text, EntityInterface $entity) {
-    // Create a content identifier.
-    $content_id = implode('.', [
-      $entity->getEntityTypeId(),
-      $entity->bundle(),
-      $entity->id(),
-    ]);
-
-    $client = new AtmClient();
-    $locked_text = $client->retrieveLockedContent($content_id);
-
-    if (empty($locked_text)) {
-      if ($client->createLockedContent($content_id, $text)) {
-        $locked_text = $client->retrieveLockedContent($content_id);
-      }
-    }
-
-    if (!empty($locked_text['Content'])) {
-      $text = $locked_text['Content'];
-    }
-
     return $text;
   }
 
