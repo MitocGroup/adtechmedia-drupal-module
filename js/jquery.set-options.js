4a2b9f687213cacee6551957a1d8e542d2f80efb
Maxim Sergienko <azzaro.max@gmail.com>
2017-02-09 00:11:34 +0200

    Made drupal lib (css/js)

---
+++ js/jquery.set-options.js
@@ -0,0 +1,28 @@
+(function ($, D) {
+
+  $.fn.setOptions = function (options) {
+    var $this = $(this)
+
+    if ($this[0].tagName !== 'SELECT') {
+      return
+    }
+
+    $this.html(
+      $('<option/>', {
+        'html': D.t('- Select -')
+      })
+    )
+
+    for (var value in options) {
+      if (options.hasOwnProperty(value)) {
+        $this.append(
+          $('<option/>', {
+            'value': value,
+            'html': options[value]
+          })
+        )
+      }
+    }
+  }
+
+})(jQuery, Drupal)
